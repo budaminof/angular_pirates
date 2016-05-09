4d0535ff4882e79b467e8d9a79e78a8bf1d80f02
bud aminof <budaminof@gmail.com>
2016-05-09 17:16:39 -0600

    white space

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')[development]);
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  knex('pirates')
+  .then(function(data){
+    
+  })
 });
 
 module.exports = router;
